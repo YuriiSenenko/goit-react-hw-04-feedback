94710aab666d2eeb26126d8ce8ee4c99ec2bfdcd
YuriiSenenko <yurkosenenko@gmail.com>
2022-11-22 11:39:32 +0200

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,15 +16,15 @@ export function App() {
   const increment = event => {
     switch (event.currentTarget.name) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
 
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
 
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
 
       default:
